9aaee93d320fcc49360f00eeb7d3532e5b62cb6d
Sameer Iqbal <sameeriqbal682@gmail.com>
2024-12-12 09:16:27 +0530

    handle input form

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./SearchBox.css";
 
 function SearchBox() {
+  let [city, setCity] = useState("");
+  let handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
+  let handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(city);
+    setCity("");
+  };
   return (
     <div className="SearchBox">
       <h3>Search for the weather</h3>
-      <form>
-        <TextField id="city" label="City Name" variant="outlined" required />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="city"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <Button variant="contained" type="submit">
