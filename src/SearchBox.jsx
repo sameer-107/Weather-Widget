9621aba19892736bb52c1eff3a1d586feed96bcd
Sameer Iqbal <sameeriqbal682@gmail.com>
2024-12-12 09:49:55 +0530

    add API

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,6 +5,17 @@ import "./SearchBox.css";
 
 function SearchBox() {
   let [city, setCity] = useState("");
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "a0eb833299501c22acbf92744f310559";
+
+  let getWeatherInfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+  };
+
   let handleChange = (evt) => {
     setCity(evt.target.value);
   };
@@ -12,6 +23,7 @@ function SearchBox() {
     evt.preventDefault();
     console.log(city);
     setCity("");
+    getWeatherInfo();
   };
   return (
     <div className="SearchBox">
