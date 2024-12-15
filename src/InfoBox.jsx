58f1d9dd303c1e0a7c0ae981fe26c40a36763a18
Sameer Iqbal <sameeriqbal682@gmail.com>
2024-12-15 10:27:00 +0530

    Adding some feature and Icons

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -3,24 +3,45 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import "./InfoBox.css";
 
-function InfoBox({info}) {
+function InfoBox({ info }) {
   const INIT_URL =
     "https://media.istockphoto.com/id/498063665/photo/rainy-landscape.webp?a=1&b=1&s=612x612&w=0&k=20&c=hOE6L7f7OoSKUW1Q4tR27GoEkOU_ywKJGCvSO77SeZg=";
-
+  const HOT_URL =
+    "https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8SG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+  const COLD_URL =
+    "https://plus.unsplash.com/premium_photo-1670604649107-a0171e5f1bd0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const RAIN_URL =
+    "https://media.istockphoto.com/id/1257951336/photo/transparent-umbrella-under-rain-against-water-drops-splash-background-rainy-weather-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=sw_CRZcGopaGHDWqtT1M8y64k5uCcq-nro55Bw3YzyQ=";
   return (
     <div className="InfoBox">
       <div className="cardContainer">
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             sx={{ height: 140 }}
-            image={INIT_URL}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
             title="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              {info.city}
+              {info.city}{" "}
+              {info.humidity > 80 ? (
+                <ThunderstormIcon />
+              ) : info.temp > 15 ? (
+                <WbSunnyIcon />
+              ) : (
+                <AcUnitIcon />
+              )}
             </Typography>
             <Typography
               variant="body2"
